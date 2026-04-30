e17411a0113ef09a8748a99c14640570534e1fc8
mostofa89 <mostofakamaljoy89@gmail.com>
2026-04-30 22:02:15 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,4 +1,14 @@
 import datetime
+from calculator import add, subtract, multiply, divide
 
 print("Name: Mostofa Kamal Joy")
 print("Date:", datetime.date.today())
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+
+    print(f"Addition: {a} + {b} = {add(a, b)}")
+    print(f"Subtraction: {a} - {b} = {subtract(a, b)}")
+    print(f"Multiplication: {a} * {b} = {multiply(a, b)}")
+    print(f"Division: {a} / {b} = {divide(a, b)}")
