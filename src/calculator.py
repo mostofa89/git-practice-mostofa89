6a3d73eba677dd8159ea7b40b805c9788713d802
mostofa89 <mostofakamaljoy89@gmail.com>
2026-04-30 22:06:15 +0600

    Delete Multiplication Function

--- src/calculator.py
+++ src/calculator.py
@@ -5,11 +5,3 @@ def subtract(a, b):
     return a - b
 
 
-def multiply(a, b):
-    return a * b
-
-
-def divide(a, b):
-    if b == 0:
-        raise ValueError("Cannot divide by zero")
-    return a / b
